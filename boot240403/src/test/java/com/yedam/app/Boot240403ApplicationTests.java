695bb3206b99e4373469a620db795a4dadc16454
JungYouhwan <qjdxjd0643@naver.com>
2024-04-05 17:58:26 +0900

    04-05

--- boot240403/src/test/java/com/yedam/app/Boot240403ApplicationTests.java
+++ boot240403/src/test/java/com/yedam/app/Boot240403ApplicationTests.java
@@ -1,13 +1,40 @@
 package com.yedam.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
 
 @SpringBootTest
 class Boot240403ApplicationTests {
-
-	@Test
+	@Autowired
+	EmpMapper empMapper;
+	
+	//@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	void empAllList() {
+		List<EmpVO> list = empMapper.selectEmpAll();
+		assertTrue(!list.isEmpty()); // assertTrue = 지금 넘겨준게 트루인지 아닌지 체크
+	}
+	
+	@Test
+	void empInsert() {
+		EmpVO empVO = new EmpVO();
+		empVO.setLastName("Hong");
+		empVO.setEmail("kdHong@goo.com");
+		empVO.setJobId("IT_PROG");
+		
+		int result = empMapper.insertEmp(empVO);
+		assertEquals(1, result);
+	}
 }
